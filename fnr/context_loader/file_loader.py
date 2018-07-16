515e146409f575d6bc862b3aa8ad862221787ca3
Collin Choy <Collin.Choy@nielsen.com>
2018-07-16 17:03:10 -0400

    Basic file not found error handling for file_loader

--- fnr/context_loader/file_loader.py
+++ fnr/context_loader/file_loader.py
@@ -1,10 +1,21 @@
 class file_loader:
     def __init__(self, f):
         self.file = f
+        self.file_format = 'csv'
 
-    def __file_not_found_handler__(self):
-        print('File not found')
+    def __file_not_found_handler(self):
+        print('Err: Input file ({}) not found'.format(self.file) )
 
     def show(self):
-        with open(self.file) as context_file:
-            print(context_file.read())
+        try:
+            with open(self.file) as context_file:
+                print(context_file.read())
+        except FileNotFoundError:
+            self.__file_not_found_handler()
+
+    def get_context_vars(self):
+        try:
+            with open(self.file) as context_file:
+                return {row.split(',')[0]:row.strip().split(',')[1] for row in context_file}
+        except FileNotFoundError:
+            self.__file_not_found_handler()

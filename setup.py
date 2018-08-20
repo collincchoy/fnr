79f22f065c58ebcd395915fcad6b548c20afb38a
Collin Choy <collin@collins-mbp.attlocal.net>
2018-08-19 19:58:58 -0500

    Add license and author info

--- setup.py
+++ setup.py
@@ -4,6 +4,12 @@ setup(
         name = 'fnr',
         version = '0.1.1',
         packages = ['fnr'],
+        author = 'Collin Choy',
+        author_email = 'collinchoyccc@gmail.com',
+        description = 'A command line utility to find & replace text.'
+        classifiers=[
+            'Programming Language :: Python :: 3'
+        ],
         test_suite = 'fnr.tests.fnrTests',
         setup_requires = ['nose>=1.0'],
         entry_points = {

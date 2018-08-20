5babe26cf31d47cae003378d9e994b1e1de071eb
Collin Choy <collin@collins-mbp.attlocal.net>
2018-08-19 21:08:36 -0500

    Update setup and readme for first deployment to PyPi

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 from setuptools import setup
 
+with open("README.md", 'r') as ld:
+    long_description = ld.read()
+
 setup(
         name = 'fnr',
-        version = '0.1.1',
+        version = '0.1.2',
         packages = ['fnr'],
         author = 'Collin Choy',
         author_email = 'collinchoyccc@gmail.com',
-        description = 'A command line utility to find & replace text.'
+        description = 'A command line utility to find & replace text.',
+        long_description=long_description,
+        url='https://github.com/collincchoy/fnr',
         classifiers=[
             'Programming Language :: Python :: 3'
         ],
